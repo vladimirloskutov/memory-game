bc4764a7aeb3a86eafb8ad86a5b61cb954006648
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-22 19:02:36 +0300

    add Timer component

---
+++ src/components/Timer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class Timer extends React.Component {
+  render() {
+    return (
+        <div className="h1 d-inline ml-5">0:00:00</div>
+    );
+  }
+}
