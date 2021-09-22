abcf1ab065626271309adf2f2f93bd24be3afb91
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-22 20:26:14 +0300

    small fixes in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ControlPanel from "./ControlPanel";
-import StartButton from "./StartButton";
-import Timer from "./Timer";
 import CardsBoard from "./CardsBoard";
 
 export default class App extends React.Component {
   render() {
     return (
         <div className="app w-75 mx-auto">
-          <ControlPanel>
-            <StartButton />
-            <Timer />
-          </ControlPanel>
+          <ControlPanel />
           <CardsBoard />
         </div>
     );
