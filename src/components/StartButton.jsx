36b9c2eb73a6f9a850a1ed81b3fb8f6fcd8c5a6f
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-22 19:01:30 +0300

    add StartButton component

---
+++ src/components/StartButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class StartButton extends React.Component {
+  render() {
+    return (
+      <button type="button" className="btn btn-primary btn-lg ml-5">START</button>
+    );
+  }
+}
