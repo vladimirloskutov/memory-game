2160a425eda749a3a221b82a120535d201f562e6
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-22 18:59:59 +0300

    add CardsBoard component. Import Card into it

---
+++ src/components/CardsBoard.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import Card from "./Card";
+
+export default class CardsBoard extends React.Component {
+  render() {
+    return (
+      <div className="cards-board ml-5">
+        <div className="row mb-3">
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+          <div className="col-2">
+            <Card />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
