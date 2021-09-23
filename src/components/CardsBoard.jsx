4eccc71731f6b336dc6b5ca08af7a00c1a7ba4bc
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-23 15:05:02 +0300

    add defaultProps and constructor to CardsBoard component

--- src/components/CardsBoard.jsx
+++ src/components/CardsBoard.jsx
@@ -2,16 +2,35 @@ import React from "react";
 import Card from "./Card";
 
 export default class CardsBoard extends React.Component {
+  static defaultProps = {
+    icons: [
+      { value: '1', status: 'closed' }, { value: '2', status: 'closed' }, { value: '3', status: 'closed' }, { value: '4', status: 'closed' },
+      { value: '5', status: 'closed' }, { value: '6', status: 'closed' }, { value: '7', status: 'closed' }, { value: '8', status: 'closed' },
+      { value: '9', status: 'closed' }, { value: '1', status: 'closed' }, { value: '2', status: 'closed' }, { value: '3', status: 'closed' },
+      { value: '4', status: 'closed' }, { value: '5', status: 'closed' }, { value: '6', status: 'closed' }, { value: '7', status: 'closed' },
+      { value: '8', status: 'closed' }, { value: '9', status: 'closed' }, { value: '!', status: 'closed' }, { value: '@', status: 'closed' },
+      { value: '#', status: 'closed' }, { value: '$', status: 'closed' }, { value: '%', status: 'closed' }, { value: '^', status: 'closed' },
+      { value: '&', status: 'closed' }, { value: '*', status: 'closed' }, { value: '-', status: 'closed' }, { value: '!', status: 'closed' },
+      { value: '@', status: 'closed' }, { value: '#', status: 'closed' }, { value: '$', status: 'closed' }, { value: '%', status: 'closed' },
+      { value: '^', status: 'closed' }, { value: '&', status: 'closed' }, { value: '*', status: 'closed' }, { value: '-', status: 'closed' },
+    ],
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shuffledIcons: props.icons.sort(() => Math.random() - 0.5),
+    };
+  }
+
   renderCardsBoard(items) {
     return items.map((item, index) => <Card data={item} key={index} />);
   }
 
   render() {
-    const shuffledIcons = this.props.icons.sort(() => Math.random() - 0.5);
-
     return (
       <div className="cards-board row ml-5">
-        {this.renderCardsBoard(shuffledIcons)}
+        {this.renderCardsBoard(this.state.shuffledIcons)}
       </div>
     );
   }
