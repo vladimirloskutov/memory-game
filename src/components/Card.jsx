3e0d2d2e2cb2d1dfe33466be1b677d2ebc7b57e8
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-23 15:13:33 +0300

    update render method. add statuses of Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,41 @@ import React from "react";
 
 export default class Card extends React.Component {
   render() {
-    return (
-      <div className="col-2 mb-4">
-        <div className="card bg-warning">
-          <div className="card-body"></div>
-        </div>
-      </div>
-    );
+    const { id, value, status } = this.props.data;
+    let card;
+
+    switch (status) {
+      case 'closed':
+        card = (
+            <div className="col-2 mb-4">
+              <div className="card bg-warning">
+                <div id={id} className="card-body" onClick={this.handleClick}></div>
+              </div>
+            </div>
+        );
+        break;
+      case 'opened':
+        card = (
+            <div className="col-2 mb-4">
+              <div className="card">
+                <div id={id} className="card-body" onClick={this.handleClick}>{value}</div>
+              </div>
+            </div>
+        );
+        break;
+      case 'deleted':
+        card = (
+            <div className="col-2 mb-4">
+              <div className="card bg-white">
+                <div id={id} className="card-body" onClick={this.handleClick}></div>
+              </div>
+            </div>
+        );
+        break;
+      default:
+        throw new Error(`Unknown card status: ${status}`);
+    }
+
+    return card;
   }
 }
