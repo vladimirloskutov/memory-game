6dd22c52458c725f240906da42f300db1425610c
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-23 12:00:40 +0300

    add state with icons values to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,18 @@ import ControlPanel from "./ControlPanel";
 import CardsBoard from "./CardsBoard";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      icons: [
+        '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '!', '@', '#', '$', '%', '^', '&', '*', '-',
+        '!', '@', '#', '$', '%', '^', '&', '*', '-',
+      ],
+    };
+  }
+
   render() {
     return (
         <div className="app w-75 mx-auto">
