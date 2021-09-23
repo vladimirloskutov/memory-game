ff6ceb8a6b32c4fa77a13c320a3fb3e49df07b78
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-23 12:05:15 +0300

    add shuffledIcons function and CardsBoard render

--- src/components/CardsBoard.jsx
+++ src/components/CardsBoard.jsx
@@ -2,57 +2,16 @@ import React from "react";
 import Card from "./Card";
 
 export default class CardsBoard extends React.Component {
+  renderCardsBoard(items) {
+    return items.map((item, index) => <Card value={item} key={index}/>);
+  }
+
   render() {
+    const shuffledIcons = this.props.icons.sort(() => Math.random() - 0.5);
+
     return (
-      <div className="cards-board ml-5">
-        <div className="row mb-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <div className="row mb-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <div className="row mb-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <div className="row mb-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <div className="row mb-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <div className="row mb-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+      <div className="cards-board row ml-5">
+        {this.renderCardsBoard(shuffledIcons)}
       </div>
     );
   }
