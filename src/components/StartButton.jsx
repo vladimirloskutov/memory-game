86d9ea837b97f71a0698d6d687ee0a86e1b780a9
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-25 13:51:11 +0300

    small fix

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class StartButton extends React.Component {
   render() {
     return (
-      <button type="button" className="btn btn-primary btn-lg ml-5">START</button>
+      <button type="button" className="btn btn-primary btn-lg">START</button>
     );
   }
 }
