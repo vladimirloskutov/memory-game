f9c3293e07bed7328294531ff3b44b513a3629bb
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-25 13:50:26 +0300

    small fix

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class Timer extends React.Component {
   render() {
     return (
-        <div className="h1 d-inline ml-5">0:00:00</div>
+        <div className="h1 d-inline">0:00:00</div>
     );
   }
 }
