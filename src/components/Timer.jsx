c1d13711f3c8746dbd58dd4960eb17ca693c4464
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-09-27 00:50:20 +0300

    small fix

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,7 +4,7 @@ export default class Timer extends React.Component {
   render() {
     const { gameTimer } = this.props;
     return (
-        <div className="d-inline h1">Time: {gameTimer} s</div>
+        <div className="d-inline h2">Time: {gameTimer} s</div>
     );
   }
 }
